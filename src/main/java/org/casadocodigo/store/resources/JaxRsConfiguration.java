1ad81de8d7b9bd5afdd3943e8332242a92c7083d
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-05-09 11:50:37 -0300

    cap 8.8 Configurando o JAX-RS

---
+++ src/main/java/org/casadocodigo/store/resources/JaxRsConfiguration.java
@@ -0,0 +1,9 @@
+package org.casadocodigo.store.resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/services")
+public class JaxRsConfiguration extends Application {
+}
+
